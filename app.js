4a80570860b750c73a58b3f9ee1a951e04f08b3c
Ankit Maewall <Ankit.Maewall@gmail.com>
2018-01-02 00:53:58 -0500

    Module 3 finished with Bootstrapped bower -try yarn

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var express = require('express');
-var hi = 'hello'
 
-if (true) {
-  console.log('hi');
-}
-console.log(hi);
+var app = express();
+
+var port = 5000;
+
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+app.get('/',function(req, res){
+  res.send("Hello World");
+});
+
+app.get('/books',function(req, res){
+  res.send("Hello Books");
+});
+
+app.listen(port, function(err){
+  console.log("Running server on port: " + port);
+});
+
